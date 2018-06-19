b48d65b256be216ae30ea3287b1ae92931a452eb
Sarbjot Singh <sarbjot_14@hotmail.com>
2018-06-19 00:15:53 -0700

    phones

---
+++ phoneServer.js
@@ -0,0 +1,200 @@
+// users-api
+
+var express = require('express');
+var app = express();
+var serverIndex = require('serve-index');
+var http = require('http');
+
+var MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb://sarb:Sarbjot22@ds121599.mlab.com:21599/mydb";
+
+
+var port = process.env.PORT || 8080;
+var users = [];
+
+MongoClient.connect(url, function(err, client){
+  if (err) console.log(err);
+  //console.log('connected');
+  var database = client.db('mydb'); // use
+
+  var phones = database.collection('phones'); // db.documents
+  var selections = database.collection('selections');
+
+
+
+// parsing body
+app.use(express.json());
+app.use(express.urlencoded( { extended:false} ));
+
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm','html'],
+  index: "start.html"
+}
+
+app.use('/', function(req,res,next){
+  console.log(req.method, 'request:', req.url, JSON.stringify(req.body));
+  next();
+});
+
+// app.all('/', function(req,res,next){
+//   console.log('Accessing root folder');
+//   next();
+// });
+
+app.use('/', express.static('./public', options));
+
+app.get('/', function(req,res,next){
+  res.sendFile(__dirname + '/index.html');
+
+});
+
+
+
+
+app.get('/admin', function(req,res,next){
+  res.sendFile(__dirname + '/admin.html');
+
+});
+
+
+app.post('/current', function(req,res,next){
+    // console.log("currrrent and price??")
+    selections.remove();
+    var current= req.body;
+
+   //console.log(req.body);
+   // console.log(current.price);
+   phones.find({}).toArray(function(err, result) {
+     if (err) throw err;
+     //console.log(result);
+
+
+     result.forEach(function(cell){
+
+
+       ///////////price
+       if(Math.abs(current.price - cell.price)<=3){
+         //console.log(cell);
+         cell.score= +cell.score + 15;
+
+       }
+       else if(Math.abs(current.price - cell.price)<=4){
+         // console.log( Math.abs(current.price - cell.price));
+         // console.log("yay close");
+         // console.log(cell);
+         cell.score= +cell.score + 5;
+       }
+       //SCREEN
+       if(current.screen==cell.screen){
+         cell.score= +cell.score + 20;
+       }
+       else if(cell.screen== "<4"){
+         cell.score= +cell.score -10;
+       }
+       //Resolution
+       if(current.res==cell.res){
+         cell.score= +cell.score + 10;
+       }
+       else if(current.res== ">720"){
+         cell.score= +cell.score +10;
+       }
+       else if(current.res== ">1080"){
+         if(cell.res==">2k"){
+           cell.score= +cell.score +10;
+         }
+       }
+       //battery
+       if(current.battery==cell.battery){
+         cell.score= +cell.score + 15;
+       }
+       //Camera
+
+       if(Math.abs(current.camera - cell.camera)<=10){
+         //console.log(cell);
+
+         cell.score= +cell.score + 18;
+
+       }
+       else if(Math.abs(current.camera - cell.camera)<=20){
+         // console.log( Math.abs(current.price - cell.price));
+         // console.log("yay close");
+         // console.log(cell);
+         cell.score= +cell.score + 7;
+       }
+       //memory
+
+       if(current.microSD== cell.microSD){
+         cell.score= +cell.score + 15;
+       }
+
+
+       if(current.memory>= cell.memory){
+         //console.log("yes it is more");
+         cell.score= +cell.score + 15;
+
+       }
+       //Preferred
+
+       console.log(current);
+       if (current.company.constructor != Array){
+         if(current.company==cell.company){
+           cell.score= +cell.score + 10;
+         }
+        }
+        else{
+
+         current.company.forEach(function(comp){
+           if(comp==cell.company){
+             cell.score= +cell.score + 10;
+           }
+         });
+
+       }
+
+
+
+       selections.insert(cell, function(err, result){
+         //console.log(result);
+       });
+
+     })
+
+   });
+  // users.push(req.body);
+  res.sendFile(__dirname + '/results.html');
+});
+
+
+
+app.post('/newPhone', function(req,res,next){
+  console.log(req.body);
+  //users.push(req.body);
+  //console.log("prooly and error"+req.body);
+  var aPhone= req.body;
+  phones.insert(aPhone, function(err, result){
+    console.log(result);
+  });
+
+
+  res.sendFile(__dirname + '/admin.html');
+
+});
+
+app.post('/selections', function(req,res,next){
+
+  selections.find({}).toArray(function(err, result) {
+    if (err) throw err;
+    //console.log(result);
+    res.json(result)
+  });
+
+});
+
+
+});
+
+http.createServer(app).listen(port);
+console.log('running on port',port);
---
+++ public/js/results.js
@@ -0,0 +1,124 @@
+
+
+console.log("hi");
+
+
+function changing(data){
+  console.log("is the ajax not running?");
+  var theSelections= data;
+  //sort by score
+  function compare(a,b)
+ {
+         if (a.score > b.score) {
+               return -1;
+         } else   if (a.score < b.score)  {
+               return 1;
+         } else {
+               return 0;
+          }
+  }
+
+  theSelections.sort(compare);
+
+
+  console.log(theSelections);
+
+
+
+  var table = document.createElement('table');
+  table.setAttribute("id", "table");
+
+  for (var i = 0; i < theSelections.length; i++){
+      //console.log(theSelections.score);
+
+      var trlong = document.createElement('tr');
+      trlong.setAttribute("id", "trlong");
+
+      var tdRank = document.createElement('td');
+      var tdModel = document.createElement('td');
+
+      var rank = document.createTextNode(i+1);
+
+      var model = document.createTextNode(theSelections[i].model);
+      //var text3 = document.createTextNode('Text3');
+
+      tdRank.appendChild(rank);
+      tdRank.setAttribute("id", "tdRank");
+      tdModel.appendChild(model);
+      tdModel.setAttribute("id", "tdModel");
+
+      trlong.appendChild(tdRank);
+      trlong.appendChild(tdModel);
+
+
+      table.appendChild(trlong);
+
+
+      var tr = document.createElement('tr');
+      tr.setAttribute("id", "tr");
+
+
+      var td1 = document.createElement('td');
+      var td2 = document.createElement('td');
+      var td3 = document.createElement('td');
+
+      var pic = document.createElement("IMG");
+
+      var imgName= theSelections[i].imgName //"nexus5x.jpeg";
+      pic.setAttribute("src", "./images/"+imgName);
+      pic.setAttribute("id", "pic");
+      pic.width= "200"
+      //pic.setAttribute("width", "30px");
+
+//<a href="http://maps.google.com" target="_blank">.dkjlsdjsdk..</a>
+
+      var review = document.createElement("form");
+      review.setAttribute("action", theSelections[i].review);
+      review.setAttribute("target", "_blank");
+
+      var theButton= document.createElement("input");
+      theButton.setAttribute("type","submit");
+      theButton.setAttribute("value","See in Deph Review");
+      theButton.setAttribute("class","ghost-button");
+      theButton.setAttribute("target","_blank");
+
+      review.appendChild(theButton);
+
+      var link = document.createElement("form");
+      link.setAttribute("action", theSelections[i].link);
+      link.setAttribute("target", "_blank");
+
+      var theButton= document.createElement("input");
+      theButton.setAttribute("type","submit");
+      theButton.setAttribute("value","Find on Amazon");
+      theButton.setAttribute("class","ghost-button");
+      theButton.setAttribute("target","_blank");
+
+
+      link.appendChild(theButton);
+
+
+      td1.appendChild(pic);
+      td2.appendChild(review);
+      td2.setAttribute("width","22%");
+      td2.appendChild(link);
+      td2.setAttribute("width","18%");
+
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      //tr.appendChild(td3);
+
+      table.appendChild(tr);
+  }
+  document.getElementById("theTitle").appendChild(table);
+  //document.body.appendChild(table);
+}
+
+$( document ).ready(function() {
+  $.ajax({
+    method: 'post',
+    url: '/selections',
+    //data: 'current_check_string='+$('#check_in_sting').val(),
+    success: changing
+  });
+});
